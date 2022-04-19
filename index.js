3a11acc4c131ec3cdf7f46426329bf6c8dbda041
Sagiri <98183444+ISagiriI@users.noreply.github.com>
2022-04-19 20:35:27 +0000

    FirstCmd

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-Аоа
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/', function(request, response){ response.send(`Монитор активен. Локальный адрес: http://localhost:${port}`); });
+app.listen(port, () => console.log());
+const Discord = require('discord.js');
+const { Client, Intents, MessageActionRow, MessageButton, GuildMemberRoleManager } = require('discord.js');
+let client; {
+  client = new Discord.Client({
+    partials: ['MESSAGE', 'REACTION', 'CHANNEL'],
+    intents: [
+      "GUILDS",
+      "GUILD_MEMBERS",
+      "GUILD_VOICE_STATES",
+      "GUILD_PRESENCES",
+      "GUILD_MESSAGES",
+      "DIRECT_MESSAGES",
+    ],
+     allowedMentions: ['repliedUser'],
+  })
+}
+
+client.on('messageCreate', msg =>
+{
+///////////Команда 1//////////
+  if (msg.content.toLowerCase() == 'аоа' ) 
+  {
+    msg.channel.send('Аоа?')
+  }
+})
+
+
+// TipoToken| 
+client.login(process.env.TipoToken);
+
+
